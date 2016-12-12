f95a692f2c30ab91a164163566efe2e44c60c7e6
ifaizullin <ifaizullin@test.local>
2016-12-12 13:28:03 +0300

    Лекция 2.3

---
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            app.Navigator.GoToHomePage();
+            app.Auth.Login(new AccountData("admin", "secret"));
+            app.Navigator.GoToGroupPage();
+            app.Groups.InitGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "sss";
+            group.Footer = "ddd";
+            app.Groups.FillGroupForm(group);
+            app.Groups.SubmitGroupCreation();
+            app.Groups.ReturnToGroupsPage();
+        }                  
+    }
+}
